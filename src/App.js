a7deb6b675cf57bda24796893a75b27050e8ab21
dheeraj <dheerajkarwasra42@gmail.com>
2021-06-28 01:16:38 -0700

    Added react-router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
-import React from "react";
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 
 function App() {
-  return <div>hii</div>;
+  return (
+    <Switch>
+      <Route exact path="/">
+        This is home page
+      </Route>
+      <Route exact path="/starred">
+        This is STARRED
+      </Route>
+      <Route>This is 404 PAGE!!!!!!</Route>
+    </Switch>
+  );
 }
 
 export default App;
